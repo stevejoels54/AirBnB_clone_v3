b9d0714b4222ae56f4a02c5a32557c3c36920cc5
joelsteven <joelofelectronics@gmail.com>
2023-10-31 05:02:27 +0300

    Added API config

---
+++ api/v1/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Flask application methods used to connect to API """
+
+from flask import Flask, Blueprint, jsonify
+from api.v1.views import app_views
+from models import storage
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
+cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
+@app.teardown_appcontext
+def teardown_app(code):
+    """ Closes storage """
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """ 404 page not found """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    app.run(host=host, port=port, threaded=True)
