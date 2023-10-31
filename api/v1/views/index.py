fbc026262ee1d15222fb6c1a299afeb0246bbba6
joelsteven <joelofelectronics@gmail.com>
2023-10-31 05:23:33 +0300

    Added stat count view

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,27 @@
 """ json statuses from app_views routes  """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat_count():
+    """ endpoint that retrieves the # of each objects by type """
+    count_stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(count_stats)
+
+
+if __name__ == "__main__":
+    pass
